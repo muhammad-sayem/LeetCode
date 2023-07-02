7c17ed6e48fadfe964bf1b9f18a3d2674075b3d0
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-07-03 03:40:09 +0600

    first commit

---
+++ 9_Palindrome_Number_9.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        int i = 0;
+        int j = str.size()-1;
+
+        while(i<j){
+            if(str[i] != str[j]){
+                return false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
+
+
+// --------------------------------------------------------------------- //
+
+// Code of har_preet //
+
+/*
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+            int num = x;
+        
+        if(x<0){
+            return false;
+        }
+        else{
+            unsigned int rev=0;
+            while(x!=0){
+                 int rem = x % 10;
+                 rev=rev*10 + rem ;
+                 x = x/10;
+            }
+            if(num==rev){
+                 return true;
+            }
+        
+            else{
+                 return false ;
+            } 
+        }   
+
+    }
+};
+*/
